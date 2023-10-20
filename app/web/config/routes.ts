b8e371d28a50bacc131bd875b6f92177c66d8a41
AgoniMou <2627045617@qq.com>
2023-10-20 18:27:43 +0800

    修改后eeg+umi+antd

--- app/web/config/routes.ts
+++ app/web/config/routes.ts
@@ -3,39 +3,52 @@ import {IRoute} from "@umijs/core";
 export const routes: IRoute[] = [
   {
     path: '/',
-    layout: false,
+    redirect: '/',
     routes: [
       {
         path: '/',
+        redirect: '/index',
+      },
+      {
+        path: '/index',
         component: '@/layouts/BasicLayout',
-        routes: [
-          {
-            name: '首页',
-            path: '/',
-            component: '@/pages/home',
-          },
-          {
-            name: '个人中心',
-            path: '/account',
-            routes: [
-              {
-                path: '/account',
-                redirect: '/account/info',
-              },
-              {
-                name: '个人资料',
-                path: './info',
-                component: '@/pages/index',
-              },
-              {
-                name: '我的收藏',
-                path: './favour',
-                component: '@/pages/me',
-              }
-            ]
-          }
-        ],
       }
     ]
   },
+  {
+    path: '/home',
+    name: 'home',
+    routes: [
+      {
+        path: '/home',
+        redirect: '/home/index',
+      },
+      {
+        name: '首页',
+        path: '/home/index',
+        component: '@/pages/home',
+      },
+    ]
+  },
+  {
+    name: '个人中心',
+    path: '/account',
+    routes: [
+      {
+        path: '/account',
+        redirect: '/account/info',
+      },
+      {
+        name: '个人资料',
+        path: '/account/info',
+        component: '@/pages/index',
+      },
+      {
+        name: '我的收藏',
+        path: '/account/favour',
+        component: '@/pages/me',
+      }
+    ]
+  }
+
 ]
